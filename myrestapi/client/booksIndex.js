b75fd1dbb6c5d18b14ab760e8798e64e593c7c6a
Diana Olivas <dolivas644@gmail.com>
2022-09-01 20:40:49 -0700

    My rest api first setup working

--- myrestapi/client/booksIndex.js
+++ myrestapi/client/booksIndex.js
@@ -4,5 +4,20 @@ async function showBooks(){
     console.log(response);
     const responseBooks = await response.json();
     console.log(responseBooks);
+
+    for (let book of responseBooks){
+        const card = `<div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${book.isbn}</h6>
+                <div>Author: ${book.author}</div> 
+                <div>Format: ${book.format}</div>
+                <hr>
+                </div>
+            </div>
+        </div>`
+        document.getElementById('books').innerHTML = document.getElementById('books').innerHTML + card;
+    }
 }
 showBooks();
--- myrestapi/index.js
+++ myrestapi/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import books from "./books.js";
+import path from 'path';
 
 const app=express();
 const PORT = 8080;
@@ -9,15 +10,29 @@ const PORT = 8080;
 //configuring cors middleware
 app.use(cors());
 
-//const direct
 
-app.get('/',(req, resp)=> {
-    resp.send("Hello Techtonica. first REST API")
-})
+// Configuring express-prettify middleware for working with JSON
+app.use(pretty({ query: 'pretty' }));
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// to render static files from the client folder
+app.use(express.static('client'));
+
+//const direct
+const __dirname= path.resolve();
 
 app.get('/books', (req, resp)=> {
     resp.json(books);
 })
+
+
+app.get('/',(req, resp)=> {
+    //open index.html (creates route)
+    resp.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 
 
