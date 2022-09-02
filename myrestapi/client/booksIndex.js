875293fad23bbf7a88c346ed7793bfbaf98e5c24
Diana Olivas <dolivas644@gmail.com>
2022-09-02 11:07:35 -0700

    Comments

--- myrestapi/client/booksIndex.js
+++ myrestapi/client/booksIndex.js
@@ -1,3 +1,40 @@
+// const setEditModal = (isbn) => {
+//     // Get information about the book using isbn
+//     const xhttp = new XMLHttpRequest();
+
+//     xhttp.open("GET", `http://localhost:8080/book/${isbn}`, false);
+//     xhttp.send();
+
+//     const book = JSON.parse(xhttp.responseText);
+
+//     const {
+//         title,
+//         author,
+//         isbn,
+//         format
+//     } = book;
+
+//     // Filling information about the book in the form inside the modal
+//     document.getElementById('isbn').value = isbn;
+//     document.getElementById('title').value = title;
+//     document.getElementById('author').value = author;
+//     document.getElementById('isbn').value = isbn;
+//     document.getElementById('format').value = format;
+  
+
+//     // Setting up the action url for the book
+//     document.getElementById('editForm').action = `http://localhost:8080/book/${isbn}`;
+// }
+
+// const deleteBook = (isbn) => {
+//     const xhttp = new XMLHttpRequest();
+
+//     xhttp.open("DELETE", `http://localhost:8080/book/${isbn}`, false);
+//     xhttp.send();
+
+//     // Reloading the page
+//     location.reload();
+// }
 
 async function showBooks(){
     const URL= "http://localhost:8080/api/books";
--- myrestapi/index.js
+++ myrestapi/index.js
@@ -44,4 +44,52 @@ app.get("/book", (req, res) => {
   res.json("book");
 });
 
+// app.get('/book/:isbn', (req, res) => {
+//   // Reading isbn from the URL
+//   const isbn = req.params.isbn;
+
+//   // Searching books for the isbn
+//   for (let book of books) {
+//       if (book.isbn === isbn) {
+//           res.json(book);
+//           return;
+//       }
+//   }
+
+//   // Sending 404 when not found something is a good practice
+//   res.status(404).send('Book not found');
+// });
+
+// app.delete('/book/:isbn', (req, res) => {
+//   // Reading isbn from the URL
+//   const isbn = req.params.isbn;
+
+//   // Remove item from the books array
+//   books = books.filter(i => {
+//       if (i.isbn !== isbn) {
+//           return true;
+//       }
+//       return false;
+//   });
+
+//   res.send('Book is deleted');
+// });
+
+// app.post('/book/:isbn', (req, res) => {
+//   // Reading isbn from the URL
+//   const isbn = req.params.isbn;
+//   const newBook = req.body;
+
+//   // Remove item from the books array
+//   for (let i = 0; i < books.length; i++) {
+//       let book = books[i]
+//       if (book.isbn === isbn) {
+//           books[i] = newBook;
+//       }
+//   }
+
+//   res.send('Book is edited');
+// });
+
+
 app.listen(PORT, () => console.log(`HOLA! Server running at ${PORT}`));
