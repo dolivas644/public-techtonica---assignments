172fd9f3a27fe4f455c43ab91eda4f2867948ac7
Diana Olivas <dolivas644@gmail.com>
2022-09-12 13:45:42 -0700

    Get request working

--- quizApp/server/index.js
+++ quizApp/server/index.js
@@ -1,9 +1,33 @@
 import express from "express";
 import cors from "cors";
+import fetch from "node-fetch";
 
+//set config
+import { config } from "dotenv";
+config();
+
+//set express & port
 const app = express();
 const PORT = 8080;
 
+//set cors
 app.use(cors());
+
+//route for index
+app.get('/', (req,res) =>{
+    res.json("Page is running");
+})
+
+//api
+let URL = `${process.env.API_HOST}`;
+
+//fetch data
+app.get('/quiz', (req, res) =>{
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        res.send(data);
+    })
+})
 
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
