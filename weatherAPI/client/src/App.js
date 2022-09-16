241a368c1f92390c9eb4845dcffe0d93b02887e6
Diana Olivas <76704309+dolivas644@users.noreply.github.com>
2022-09-16 11:12:37 -0700

    Delete weatherAPI directory

--- weatherAPI/client/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import WeatherDashboard from './components/WeatherDashboard';
-import {useState} from 'react';
-
-function App() {
-  return <WeatherDashboard />;
-}
-
-export default App;
