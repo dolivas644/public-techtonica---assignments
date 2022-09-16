a8cb9deb2a63dd83ac2bc3f9b99d1313b9e54d9e
Diana Olivas <76704309+dolivas644@users.noreply.github.com>
2022-09-16 11:12:28 -0700

    Delete myrestapi directory

--- myrestapi/client/booksIndex.js
+++
@@ -1,8 +0,0 @@
-async function showBooks(){
-    const URL= "http://localhost:8080/books";
-    const response = await fetch(URL);
-    console.log(response);
-    const responseBooks = await response.json();
-    console.log(responseBooks);
-}
-showBooks();
