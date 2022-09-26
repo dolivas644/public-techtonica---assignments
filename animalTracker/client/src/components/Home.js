b63016aa2ed9669ac0fb197a54907335c1e422a0
Diana Olivas <dolivas644@gmail.com>
2022-09-26 14:06:06 -0700

    Home page

--- animalTracker/client/src/components/Home.js
+++ animalTracker/client/src/components/Home.js
@@ -1,7 +1,16 @@
+// import {useNavigate} from "react-router-dom";
+
 const Home = () => {
+
     return(
+        <>
         <header>
             Animal Sight Tracker
         </header>
+        <button> S T A R T </button>
+        <link to="/Home"></link>
+        </>
     )
 }
+
+export default Home;
